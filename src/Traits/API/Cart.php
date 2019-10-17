b67b03a60f91e97dc1b311b2fba2f55e925793c0
Thomas Alrek <thomas@alrek.no>
2019-10-17 17:35:19 +0200

    Add API methods for updating and adding cart items

--- src/Traits/API/Cart.php
+++ src/Traits/API/Cart.php
@@ -4,6 +4,21 @@ namespace Netflex\Commerce\Traits\API;
 
 use Exception;
 use Netflex\API;
+use Netflex\Commerce\CartItem;
 
 trait Cart
-{ }
+{
+  public function addCartItem(CartItem $item = null, $id = null)
+  {
+    $id = $id ?? $this->parent->id;
+    API::getClient()
+      ->post('commerce/orders/' . $id . '/cart', $item);
+  }
+
+  public function updateCartItem(CartItem $item = null, $id = null)
+  {
+    $id = $id ?? $this->parent->id;
+    API::getClient()
+      ->put('commerce/orders/' . $id . '/cart/' . $item->id, $item);
+  }
+}
