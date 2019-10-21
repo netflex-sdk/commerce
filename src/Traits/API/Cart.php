08cb4e6fec2441704f0e824f6a9ec1a1c4aa4ea2
Thomas Alrek <thomas@alrek.no>
2019-10-21 22:44:48 +0200

    Make addCartItem also handle updates

--- src/Traits/API/Cart.php
+++ src/Traits/API/Cart.php
@@ -11,14 +11,28 @@ trait Cart
   public function addCartItem(CartItem $item = null, $id = null)
   {
     $id = $id ?? $this->parent->id;
-    API::getClient()
+
+    if (!$item->id) {
+      API::getClient()
       ->post('commerce/orders/' . $id . '/cart', $item);
+
+      return $this->parent->refresh();
+    }
+
+    return $this->updateCartItem($item, $id);
   }
 
   public function updateCartItem(CartItem $item = null, $id = null)
   {
     $id = $id ?? $this->parent->id;
+
+    if ($item->properties->entry_id) {
+      throw new Exception('fan');
+    }
+
     API::getClient()
       ->put('commerce/orders/' . $id . '/cart/' . $item->id, $item);
+
+    return $this->parent->refresh();
   }
 }
