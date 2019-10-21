c5b622a9ce1e521bb26dc57b35dce647f4b84efa
Thomas Alrek <thomas@alrek.no>
2019-10-21 22:44:18 +0200

    Fix properties serialization when empty

--- src/Properties.php
+++ src/Properties.php
@@ -2,7 +2,17 @@
 
 namespace Netflex\Commerce;
 
+use Exception;
 use Netflex\Support\NetflexObject;
 
 class Properties extends NetflexObject
-{ }
+{
+  public function jsonSerialize()
+  {
+    if (empty($this->attributes)) {
+      return null;
+    }
+
+    return $this->attributes;
+  }
+}
