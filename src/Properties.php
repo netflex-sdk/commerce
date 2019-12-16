7d20392a2f8b5b3973df7fec03ffb5bb539ca2e1
Thomas Alrek <thomas@alrek.no>
2019-12-16 12:32:01 +0100

    Only allow scalar values

--- src/Properties.php
+++ src/Properties.php
@@ -9,13 +9,16 @@ class Properties extends NetflexObject
 {
   /** @var array */
   protected $readOnlyAttributes = [];
-  
+
+  /**
+   * @return array
+   */
   public function jsonSerialize()
   {
     $attributes = empty($this->attributes) ? [] : $this->attributes;
 
     foreach ($attributes as $key => $value) {
-      if (!is_string($value)) {
+      if (!is_scalar($value)) {
         unset($attributes[$key]);
       }
     }
