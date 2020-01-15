b75884b9e9d3f3dee6dfc2290e41e32c9a713708
tormundgerhardsen <tormund.gerhardsen@gmail.com>
2020-01-15 13:26:57 +0100

    Add PaymentItemAPI

---
+++ src/Traits/API/PaymentItemAPI.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Netflex\Commerce\Traits\API;
+
+use Exception;
+use Netflex\API;
+use Netflex\Commerce\Order;
+
+trait PaymentItemAPI
+{
+  /**
+   * @param array $updates
+   * @return static
+   * @throws Exception
+   */
+  public function save($updates = [])
+  {
+    foreach ($this->modified as $modifiedKey) {
+      $updates[$modifiedKey] = $this->{$modifiedKey};
+    }
+
+    if (!empty($updates)) {
+      API::getClient()
+        ->put(Order::basePath().$this->order_id.'/payment/'.$this->id, $updates);
+    }
+
+    $this->modified = [];
+
+    return $this;
+  }
+
+  /**
+   * @throws Exception
+   */
+  public function delete()
+  {
+    API::getClient()
+      ->delete(Order::basePath().$this->order_id.'/payment/'.$this->id);
+  }
+}
