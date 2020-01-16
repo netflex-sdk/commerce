f46741a50a02a02342d3c49061653abc50730412
tormundgerhardsen <tormund.gerhardsen@gmail.com>
2020-01-16 13:16:12 +0100

    Add support for data attribute in PaymentItem

--- src/PaymentItem.php
+++ src/PaymentItem.php
@@ -10,6 +10,44 @@ class PaymentItem extends ReactiveObject
   use PaymentItemAPI;
 
   protected $readOnlyAttributes = [
-    'id'
+    'id',
+    'order_id'
   ];
+
+  protected $timestamps = [
+    'payment_date'
+  ];
+
+  public function getOrderIdAttribute($value)
+  {
+    return (int) $value;
+  }
+
+  public function getAmountAttribute($value)
+  {
+    return (float) $value;
+  }
+
+  /**
+   * @param mixed $data
+   * @return Properties
+   */
+  public function getDataAttribute($data)
+  {
+    return Properties::factory($data, $this)
+      ->addHook('modified', function ($data) {
+        $this->__set('data', $data->jsonSerialize());
+      });
+  }
+
+  /**
+   * @return array
+   */
+  public function jsonSerialize()
+  {
+    $json = parent::jsonSerialize();
+    $json['data'] = $this->data->jsonSerialize();
+
+    return $json;
+  }
 }
