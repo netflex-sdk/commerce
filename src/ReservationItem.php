19871d7894b6cbf109ea6fce8f7af5fe5feed694
tormundgerhardsen <tormund.gerhardsen@gmail.com>
2020-01-16 13:31:35 +0100

    Set correct datatypes in ReservationItem

--- src/ReservationItem.php
+++ src/ReservationItem.php
@@ -6,7 +6,8 @@ use Netflex\Support\ReactiveObject;
 
 /**
  * @property int $cart_item
- * @property string $variant_id
+ * @property int $entry_id
+ * @property int $variant_id
  * @property string $reservation_start
  * @property string $reservation_end
  * @property array|object $reservation_length
@@ -15,9 +16,35 @@ class ReservationItem extends ReactiveObject
 {
   protected $readOnlyAttributes = [
     'cart_item',
+    'entry_id',
     'variant_id',
     'reservation_start',
     'reservation_end',
     'reservation_length'
   ];
+
+  protected $timestamps = [
+    'reservation_start',
+    'reservation_end'
+  ];
+
+  public function getCartItemAttribute($value)
+  {
+    return (int) $value;
+  }
+
+  public function getEntryIdAttribute($value)
+  {
+    return (int) $value;
+  }
+
+  public function getVariantIdAttribute($value)
+  {
+    return (int) $value;
+  }
+
+  public function getReservationLengthAttribute($value)
+  {
+    return (object) $value;
+  }
 }
